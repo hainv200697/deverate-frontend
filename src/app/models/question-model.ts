fe0ac43b57722c145f60d2513c2cce8e7adda1cb
thongnd1 <thongnd21@gmail.com>
2019-09-25 14:53:29 +0700

    insert question

--- src/app/models/question-model.ts
+++ src/app/models/question-model.ts
@@ -2,4 +2,6 @@ export class QuestionModel{
     QuestionID : number
     CategoryID : number
     Question : string
+    Status : string
+    Create_by: number
 }
--- src/app/services/question-api.service.ts
+++ src/app/services/question-api.service.ts
@@ -4,16 +4,17 @@ import { HttpClient } from '@angular/common/http';
     providedIn: 'root'
 })
 export class QuestionApiService {
-    URL = 'http://localhost:58810/swagger/ui/index#!/';
+    URL = 'http://localhost:58810/swagger/';
     constructor(private httpClient: HttpClient) { }
     
     getAllQuestion() {
         const API = 'Question/Question_GetAllQuestion';
         return this.httpClient.get(this.URL + API );
     }
-
+    
     insertQuestion(QuestionModel) {
-        // return this.httpClient.post(this.URL,);
+        const API = 'QuestionAPI/CreatQuestion';
+        return this.httpClient.post(this.URL+API,QuestionModel );
     }
 
 
