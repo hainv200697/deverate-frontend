8176d93b62cff53c058e60d0845686d67d05e686
Hai Nguyen <hainv54@fpt.com.vn>
2019-10-07 09:09:28 +0700

    resolve conflic

--- src/app/layout/layout-routing.module.ts
+++ src/app/layout/layout-routing.module.ts
@@ -21,7 +21,7 @@ const routes: Routes = [
             { path: 'blank-page', loadChildren: () => import('./blank-page/blank-page.module').then(m => m.BlankPageModule) },
             { path: 'profile', component: ProfileComponent },
             { path: 'changepass', component: ChangepassComponent },
-            { path: 'catalogue', loadChildren: () => import('../components/catalogue/catalogue.module').then(m => m.CatalogueModule)}
+            { path: 'catalogue', loadChildren: () => import('../components/catalogue/catalogue.module').then(m => m.CatalogueModule)},
             { path: 'manage-question', loadChildren: () => import('../components/question/ins-question/insert-question.module').then(m => m.InsertQuestionModule) },
             { path: 'manage-company', loadChildren: () => import('../components/company/ins-company/insert-company.module').then(m => m.InsertCompanyModule) }
         ]
