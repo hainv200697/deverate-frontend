90d4f896d7d97001e6c914895d6e9a6b9c82bfe5
HUY-NGUYEN\huy <huy.nguyen2@wisky.vn>
2019-10-10 19:05:36 +0700

    fix ui create config test

--- src/app/components/configuration-test/manage-configuration/manage-configuration.component.ts
+++ src/app/components/configuration-test/manage-configuration/manage-configuration.component.ts
@@ -10,6 +10,7 @@ import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 import Swal from 'sweetalert2'
 import Stepper from 'bs-stepper';
 import { from } from 'rxjs';
+import { ToastrService } from 'ngx-toastr';
 declare var $: any;
 @Component({
   selector: 'manage-configuration',
@@ -25,11 +26,13 @@ export class ManageConfigurationComponent implements OnInit {
     private configAPi: ConfigurationApiService,
     private catalogueApi: CatalogueApiService,
     private rankApi: RankApiService,
+    private toast: ToastrService,
   ) {
     this.page = 1;
     this.pageSize = 3;
   }
   startdate: Date = new Date();
+
   enddate: Date = new Date(this.startdate);
   settings1 = {
     bigBanner: true,
@@ -43,8 +46,7 @@ export class ManageConfigurationComponent implements OnInit {
     timePicker: true,
     format: 'dd-MM-yyyy hh:mm a',
     defaultOpen: false,
-    closeOnSelect: false,
-    minDateTime: new Date(this.startdate),
+    closeOnSelect: true,
   }
   private stepper: Stepper;
   index = 1;
@@ -85,11 +87,15 @@ export class ManageConfigurationComponent implements OnInit {
   answerForm: FormGroup;
 
   dropdownList = [
-    {"id" : 1, "itemName" : "huy"},
-    {"id" : 5, "itemName" : "a"},
-    {"id" : 2, "itemName" : "b"},
-    {"id" : 3, "itemName" : "tt"},
-    {"id" : 4, "itemName" : "ccc"}
+    { "id": 1, "name": "Program Skil" },
+    { "id": 5, "name": "Software Requirement Analysis" },
+    { "id": 2, "name": "Software Design Methods" },
+    { "id": 3, "name": "Technical Lead" },
+    { "id": 4, "name": "Personal Skills" },
+    { "id": 4, "name": "Personal Skills" },
+    { "id": 4, "name": "Personal Skills" },
+    { "id": 4, "name": "Personal Skills" },
+    { "id": 4, "name": "Personal Skills" },
 
   ];
   selectedItems = [];
@@ -99,7 +105,10 @@ export class ManageConfigurationComponent implements OnInit {
     selectAllText: 'Select All',
     unSelectAllText: 'UnSelect All',
     enableSearchFilter: true,
-    classes: "myclass custom-class"
+    classes: "form-control form-group",
+    labelKey: "name",
+    maxHeight: 240,
+    showCheckbox: true,
   };
 
 
@@ -119,10 +128,10 @@ export class ManageConfigurationComponent implements OnInit {
     this.dropdownList['itemName'] = ""
     this.dropdownList['isActive'] = true;
 
-    this.inputConfiguration['Totalquestion'] = "";
-    this.inputConfiguration['Duration'] = "";
-    this.inputConfiguration['StartDate'] = "";
-    this.inputConfiguration['EndDate'] = "";
+    this.inputConfiguration['Totalquestion'] = 1;
+    this.inputConfiguration['Duration'] = 15;
+    this.inputConfiguration['StartDate'] = this.startdate;
+    this.inputConfiguration['EndDate'] = this.enddate.setDate(this.startdate.getDate() + 1);
   }
 
   onItemSelect(item: any) { }
@@ -139,7 +148,6 @@ export class ManageConfigurationComponent implements OnInit {
         this.selectedItems.splice(i, 1);
       }
     }
-    console.log(this.selectedItems);
   }
 
   open(content) {
@@ -153,11 +161,13 @@ export class ManageConfigurationComponent implements OnInit {
   }
 
   next() {
+    if (this.validateConfiguration() == false) {
+      return;
+    }
     this.stepper.next();
     this.index = this.index + 1;
     this.inputConfiguration["StartDate"] = this.startdate;
     this.inputConfiguration['EndDate'] = this.enddate;
-    console.log(this.selectedItems, this.inputConfiguration);
   }
 
   back() {
@@ -179,13 +189,24 @@ export class ManageConfigurationComponent implements OnInit {
 
   }
 
+  clickButtonRefresh(refesh) {
+    refesh.classList.add('spin-animation');
+    setTimeout(function () {
+      refesh.classList.remove('spin-animation');
+    }, 500);
+    this.getConfigurationIsActive(true);
+  }
+
+  
   getConfigurationIsActive(status: boolean) {
+    this.iconIsActive = status;
     this.configAPi.getAllConfiguration(status).subscribe(
       (data) => {
         this.Configurations = data['data']['data'];
       }
     );
   }
+  
 
   checkIfAllSelected() {
     this.selectCattalogue = [];
@@ -201,12 +222,60 @@ export class ManageConfigurationComponent implements OnInit {
   }
 
   Create() {
-    console.log(this.inputConfiguration);
+    let inputAllConfig = [];
     if (this.inputConfiguration['Duration'] == "" || this.inputConfiguration['Totalquestion'] == "") {
       Swal.fire('Error', 'Something went wrong', 'error');
       return;
     }
+    inputAllConfig.push(this.inputConfiguration, this.selectedItems, this.ListRank);
     this.closeModal();
     Swal.fire('Success', 'The configuration has been created', 'success');
+    console.log(inputAllConfig);
+  }
+
+  validateConfiguration() {
+    if (this.inputConfiguration['Totalquestion'] < 1 || this.inputConfiguration['Totalquestion'] > 100) {
+      this.toast.error('Message', 'Total question must be range 1 to 100');
+      return false;
+    }
+    else if (this.inputConfiguration['Duration'] < 15 || this.inputConfiguration['Duration'] > 180) {
+      this.toast.error('Message', 'Duration must be range 15 to 200');
+      return false;
+    }
+    else if (this.selectedItems.length == 0) {
+      this.toast.error('Message', 'Please select the catalogue');
+      return false;
+    }
+    else if ($('#mark').val() == "") {
+      this.toast.error('Message', 'Please input rate of catalogue');
+      return false;
+    }
+    var total = 0;
+    for (var i = 0; i < this.selectedItems.length; i++) {
+      total = this.selectedItems[i]['mark'] + total;
+    }
+    if (total != 1) {
+      console.log(total)
+      this.toast.error('Message', 'Total mark of catalogue must be 1');
+      return false;
+    }
+
+    else if (this.index == 2) {
+      if ($('#rate').val() == "") {
+        this.toast.error('Message', 'Please input rate of rank');
+        return false;
+      }
+      // else {
+      //   var total = 0;
+      //   for (var i = 0; i < this.ListRank.length; i++) {
+      //     total = this.ListRank[i]['rate'] + total;
+      //   }
+      //   if (total != 1) {
+      //     console.log(total)
+      //     this.toast.error('Message', 'Total mark of Rank must be 1');
+      //     return false;
+      //   }
+      // }
+    }
   }
 }
