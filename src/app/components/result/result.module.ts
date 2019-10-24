31e0746677121ed868623f3a95d281c2727825dc
HUY-NGUYEN\huy <huy.nguyen2@wisky.vn>
2019-10-24 16:01:30 +0700

    fix ui result

--- src/app/components/result/result.module.ts
+++ src/app/components/result/result.module.ts
@@ -6,11 +6,24 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ResultComponent } from './result.component';
 
 import { ResultRoutingModule } from './result-routing.module';
-
 import { ChartsModule as Ng2Charts } from 'ng2-charts';
 import { GaugeModule } from 'angular-gauge';
 import { NgxGaugeModule } from 'ngx-gauge';
 import { NgxLoadingModule } from 'ngx-loading';
+import { FusionChartsModule } from 'angular-fusioncharts';
+
+// Load FusionCharts
+import * as FusionCharts from 'fusioncharts';
+
+// Load Widgets
+import * as Widgets from 'fusioncharts/fusioncharts.widgets';
+
+// Load FusionTheme Theme
+import * as FusionTheme from 'fusioncharts/themes/fusioncharts.theme.fusion'
+
+// Add dependencies to FusionChartsModule
+FusionChartsModule.fcRoot(FusionCharts, Widgets, FusionTheme);
+
 import 'd3';
 import 'nvd3';
 
@@ -27,6 +40,7 @@ import 'nvd3';
     Ng2Charts,
     NgxGaugeModule,
     GaugeModule.forRoot(),
+    FusionChartsModule,
     NgxLoadingModule.forRoot({})
   ],
   
