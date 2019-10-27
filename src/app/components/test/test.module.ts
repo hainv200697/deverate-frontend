b296ae8a6692611bec08ba6670abca3c66ce934f
Nguyen Dinh Thong <thongndse61790@fpt.edu.vn>
2019-10-27 16:14:59 +0700

    count down

--- src/app/components/test/test.module.ts
+++ src/app/components/test/test.module.ts
@@ -5,12 +5,14 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { TestComponent } from './test.component';
 import { NgxLoadingModule } from 'ngx-loading';
+import { CountdownModule } from 'ngx-countdown';
 @NgModule({
   imports: [
     CommonModule,
     NgbModule,
     FormsModule,
     TranslateModule,
+    CountdownModule, 
     ReactiveFormsModule,
     NgxLoadingModule
   ],
