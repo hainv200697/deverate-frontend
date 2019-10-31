09e2025f0106583103e1f8933e29d26e8723dfeb
Nguyen Dinh Thong <thongndse61790@fpt.edu.vn>
2019-10-31 14:15:55 +0700

    create employee

--- src/app/layout/layout-routing.module.ts
+++ src/app/layout/layout-routing.module.ts
@@ -33,6 +33,7 @@ const routes: Routes = [
             { path: 'manage-detail-test/:id', loadChildren: () => import('../components/default-test/defaut-test.module').then(m => m.DefautTestModule) },
             { path: 'reranking', component: RerankingComponent},
             { path: 'statistic', loadChildren: () => import('../components/statistic/statistic.module').then(m => m.StatisticModule)},
+            { path: 'manage-employee', loadChildren: () => import('../components/employee/employee.module').then(m => m.EmployeeModule)},
         ]
     }
 ];
